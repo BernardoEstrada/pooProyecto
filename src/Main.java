7c85cfd25a28852c3ec9e60eebeb3ce5a6ec07bc
Bernardo <a01704320@itesm.mx>
2019-04-24 14:56:56 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,5 @@ public class Main {
         VentanaMenu principal = new VentanaMenu(400,800);
         principal.setVisible(true);
         principal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        System.out.println("asdfg");
-        System.out.println(5+12%3-2);
-        System.out.println("qwerty");
     }
 }
