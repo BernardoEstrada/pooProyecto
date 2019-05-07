059650d1c01542faeae4ef69d4558109b735f29b
Bernardo <a01704320@itesm.mx>
2019-05-07 13:26:48 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, UnsupportedLookAndFeelException, IllegalAccessException {
 
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        TestVen principal = new TestVen();
+        MiVentana principal = new MiVentana();
         principal.setSize(500, 500);
         principal.setVisible(true);
         principal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
